50ec517b6663fe92b26315d6f5c9e939986b3525
stanley160990 <kokobachrudin@gmail.com>
2023-01-08 10:12:00 +0700

    Update URL

--- run.sh
+++ run.sh
@@ -3,9 +3,16 @@
 # Args 1 Bootcamp Days
 # Args 2 GPU ID
 # Args 3 Image Name
+arrVar=()
+
 for i in {0..1}
 do
 	docker run --gpus '"device='"${2}"':'"${i}"'"' -d -p $1$200$i:8888 --name bootcamp$1-$2-$i $3
 
-	echo "http://domain:$1$200$i"
+	arrVar[${#arrVar[@]}]="http://domain:$1$200$i"
+done
+
+for value in "${arrVar[@]}"
+do
+     echo $value
 done
